85c2298bee42d3c8ed662cc43300dae46e32930f
Pelicano <_victorryan1_@hotmail.com.br>
2023-07-31 18:49:18 -0300

    Python

---
+++ Python/00-variables.py
@@ -0,0 +1,11 @@
+ #Pyhton sets automaticaly the variable types
+x = 10 #int
+name = 'student' #string
+nota = 8.75 #float
+fez_incricao = True #boolean
+
+#To discover the type of a variable we can use the comand "type" together with the command "print", the output will show us the type
+print(type(x))
+print(type(name))
+print(type(nota))
+print(type(fez_incricao))
