878e09041344284ea1ade9be920ceb1c9d0d47e8
Pelicano <_victorryan1_@hotmail.com.br>
2023-07-31 22:52:40 -0300

    Studies 31/07
    Início do módulo de Linguagem de programação

--- Python/03-mathematic_operators.py
+++ Python/03-mathematic_operators.py
@@ -2,5 +2,21 @@
 in python we use
 + : plus
 - : minus
+* : multiplication
+/ : division
+// : division but stores only the integer part
+% : Retorna o resto da divisão entre operandos
+** : Retorna um número elevado a potência de outro
+pow(x, y) : returns the x elevated to y
 
-"""   
+Python follows the normal rules of mathematics
+"""
+operation_1 = 2 + 3 * 5
+operation_2 = (2 + 3) * 5
+operation_3 = 4 / 2 ** 2
+operation_4 = 13 % 3 + 4
+
+print(f"The result of the operation 1 is: {operation_1}")
+print(f"The result of the operation 2 is: {operation_2}")
+print(f"The result of the operation 3 is: {operation_3}")
+print(f"The result of the operation 4 is: {operation_4}")
